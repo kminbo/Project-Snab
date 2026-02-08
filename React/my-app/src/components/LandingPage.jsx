aad35c48fec7db670221b9d4193d347360b709ef
kminbo <kminbo214@gmail.com>
2026-02-08 00:59:55 -0500

    landing

--- React/my-app/src/components/LandingPage.jsx
+++ React/my-app/src/components/LandingPage.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import '../App.css';
-import background from '../assets/THERA.jpg';
+import logo from '../assets/logo.jpg';
+import stars from '../assets/stars.jpg';
+import tree from '../assets/tree.png';
 
 const LandingPage = ({ onEnter }) => {
   return (
     <div className="landing-page">
-      <img src={background} alt="" className="landing-bg" />
-
-      <button className="enter-button" onClick={onEnter}>
-        Let the magic begin
-      </button>
+      <img src={stars} alt="" className="landing-stars" />
+      <img src={tree} alt="" className="landing-tree" />
+      <img src={logo} alt="Thera" className="title-image" />
+      <p className="tagline">Where conversation turns into comfort.</p>
+      <div className="landing-right">
+        <button className="enter-button" onClick={onEnter}>
+          Let the magic begin
+        </button>
+      </div>
     </div>
   );
 };
